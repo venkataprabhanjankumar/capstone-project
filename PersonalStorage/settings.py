6ea2de51f996289fc414c8c935ae4ffef994ec9e
Venkata Prabhanjan Kumar <84016030+venkataprabhanjankumar@users.noreply.github.com>
2022-10-18 11:53:58 +0530

    Update settings.py

--- PersonalStorage/settings.py
+++ PersonalStorage/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-94!auh)k0gjca$5wmm_-fzcc*!%2qm1fm69^o*5z*82k3-+e9z'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
