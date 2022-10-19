8ac2b5593ef5b1d6db3bd81d964579097eece8d0
venkataprabhanjankumar <venkataprabhanjankumar@gmail.com>
2022-10-19 09:42:05 +0530

    Added File Upload

---
+++ dashboard/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.utils.timezone import datetime
+
+
+class Storage(models.Model):
+    user = models.CharField(max_length=225)
+    file_name = models.CharField(max_length=225)
+    description = models.CharField(max_length=225, blank=True)
+    access_time = models.DateTimeField(default=datetime.now())
+    file = models.FileField(upload_to='personalFiles')
---
+++ dashboard/views.py
@@ -0,0 +1,56 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
+
+# Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView, FormView, ListView
+from django.utils.timezone import datetime
+
+from .models import Storage
+from .forms import StorageForm
+
+
+# we need to specify the LoginRequiredMixin First before the Template View
+class Dashboard(LoginRequiredMixin, TemplateView):
+    template_name = 'dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['menu'] = 'dashboard'
+        context['user'] = self.request.user
+        return context
+
+
+class CreateFile(LoginRequiredMixin, FormView):
+    form_class = StorageForm
+    template_name = 'create_files.html'
+
+    def form_valid(self, form):
+        user = self.request.user.username
+        file_name = form.cleaned_data['file_name']
+        description = form.cleaned_data['description']
+        file = form.cleaned_data['file']
+        storage = Storage.objects.create(user=user, file_name=file_name, description=description,
+                                         access_time=datetime.now(), file=file)
+        storage.save()
+        return redirect(reverse_lazy('view_files'))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['menu'] = 'create_files'
+        context['user'] = self.request.user
+        return context
+
+
+class ViewFile(ListView):
+    model = Storage
+    context_object_name = 'files'
+    template_name = 'view_files.html'
+
+    def get_queryset(self):
+        return Storage.objects.filter(user=self.request.user.username).order_by('access_time')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
+        context['menu'] = 'view_file'
+        return context
